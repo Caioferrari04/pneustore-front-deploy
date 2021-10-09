c9a4b1fb249336ad809682ec4ad8fe82764d78f3
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-08 21:58:42 -0300

    commit estilizaçao <frete> 2

--- Components/Frete/Frete.js
+++ Components/Frete/Frete.js
@@ -7,9 +7,9 @@ export default function Frete(){
       <div className={styles.card}>
         <div className={styles.info}>
           <h1>Informe o seu CEP</h1>
-            <input></input>
-            <button>Calcular Entrega</button>
-            <h4>Não sei meu CEP</h4>
+          <input></input>
+          <button>Calcular Entrega</button>
+          <h4>Não sei meu CEP</h4>
         </div>
       </div>  
   )
