0d809ab2e2fc905b7f5391f9773941e88a78efaf
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-12 20:54:54 -0300

    ajuste no footer, card e itemcard

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -3,10 +3,12 @@ import styles from './Footer.module.scss';
 export default function Footer(){
     return(
         <div className={styles.footer}>
+            <div className={styles.list}>
             <h2>Institucional</h2>
             <h2>Ajuda</h2>
             <h2>Serviço</h2>
             <h2>Central de relacionamento</h2>
+            </div>
         </div>  
         
     )
