dd8a241163b85d9d8c2225dc3d16de4875136332
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-12 21:05:26 -0300

    ajuste footer e item card

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -4,12 +4,19 @@ export default function Footer(){
     return(
         <div className={styles.footer}>
             <div className={styles.list}>
-            <h2>Institucional</h2>
-            <h2>Ajuda</h2>
-            <h2>Serviço</h2>
-            <h2>Central de relacionamento</h2>
+                <ul>
+                    <li>Institucional</li>
+                    <li>Ajuda</li>
+                    <li>Serviços</li>
+                    <li>Central de relacionamentos</li>
+                </ul>
             </div>
         </div>  
         
     )
 }
+
+//<h2>Institucional</h2>
+{/* <h2>Ajuda</h2>
+<h2>Serviço</h2>
+<h2>Central de relacionamento</h2> */}
