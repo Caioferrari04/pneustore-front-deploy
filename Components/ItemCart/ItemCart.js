8d2f5f8348ef1d9a4938a44e8e38bacc5c72fb5c
AndersonRFerreira <andersonn.reis90@gmail.com>
2021-10-13 14:29:54 -0300

    estilização de alguns componentes, inserindo novos botoes

--- Components/ItemCart/ItemCart.js
+++ Components/ItemCart/ItemCart.js
@@ -7,10 +7,10 @@ export default function ItemCart(){
       <div className={styles.card}>
         <div className={styles.info}>
           <h1>Resumo do pedido</h1>
-            <h2> nome do produto </h2>
-            <h3> subtotal e valor </h3>
-            <button>Continuar comprando</button>
-            <button>finalizar compra</button>
+            <h2> Pneu Firestone 15 ExtremeContact DW 225/70R16 </h2>
+            <h3> Subtotal: R$548,43 </h3>
+            <button className={styles.button1}>Continuar comprando</button>
+            <button className={styles.button2}>finalizar compra</button>
         </div>
       </div>
           
