b0f5883cc1794f0e5bf89deeab5e1d3bf8d5ded1
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-14 23:03:56 -0300

    reajuste botao

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Tabs from '../Components/Tabs/Tabs';
 import ItemCart from '../Components/ItemCart/ItemCart';
 import Footer from '../Components/Footer/Footer';
 import Frete from '../Components/Frete/Frete';
+import Button from '@mui/material/Button';
 
 export default function Home() {
 
@@ -46,26 +47,23 @@ export default function Home() {
       <Frete/>
       <Tabs>
       <div label="Montagem Móvel">
-            <h2 className={styles.title}>Agende sua entrega com a PneuStore Móvel</h2>
-              <p className={styles.text}>
-              
-              </p>
-              <button className={styles.button2}>agendar </button>
-          </div>
-          <div label="Montagem no parceiro">
-            <h2 className={styles.title}>Entrega e montagem em um centro de montagem parceiro</h2>
-              <p className={styles.text}>
-                
-              </p>
-              <button className={styles.button2}>Selecionar parceiro</button>
-          </div>
-          <div label="Entrega no meu endereço">
-            <h2 className={styles.title}>Entrega comum em sua residência</h2>
-              <p className={styles.text}>
-              
-              </p>
-              
-          </div>
+          <h2 className={styles.title}>Agende sua entrega com a PneuStore Móvel</h2>
+          <p className={styles.text}>É fácil: você escolhe o pneu, verifica a disponibilidade do
+          serviço, agenda e pronto! Nós entregamos e montamos o pneu sem vocẽ sair de casa </p>
+          <Button className={styles.botaoAgendar}variant="outlined">AGENDAR</Button>
+      </div>
+
+      <div label="Montagem no parceiro">
+        <h2 className={styles.title}>Centro de montagem parceiro</h2>
+        <p className={styles.text}></p>
+        <button className={styles.button2}>Selecionar parceiro</button>
+      </div>
+
+      <div label="Entrega no meu endereço">
+        <h2 className={styles.title}>Entrega comum em sua residência</h2>
+        <p className={styles.text}></p>
+      </div>
+
       </Tabs>
       <ItemCart/>
       <Footer/>
