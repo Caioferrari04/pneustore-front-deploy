64bd07e5db9111136cd8d28ad1fb095f9d186be9
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-15 21:52:30 -0300

    ajustes varias paginas

--- Components/ItemCart/ItemCart.js
+++ Components/ItemCart/ItemCart.js
@@ -6,15 +6,15 @@ export default function ItemCart(){
   return(
       <div className={styles.card}>
         <div className={styles.info}>
-          <h1>Resumo do pedido</h1>
-            <h2> Pneu Firestone 15 ExtremeContact DW 225/70R16 </h2>
-            <h3> Subtotal: R$548,43 </h3>
-            <button className={styles.button1}>Continuar comprando</button>
-            <button className={styles.button2}>finalizar compra</button>
+          <div className={styles.cabecalho}>
+            <h2>Resumo do pedido</h2>
+            <img src='bag.png' />
+          </div>
+          <h3> Pneu Firestone 15 ExtremeContact DW 225/70R16 </h3>
+          <h3> Subtotal: R$548,43 </h3>
+          <button className={styles.button1}>Continuar comprando</button>
+          <button className={styles.button2}>finalizar compra</button>
         </div>
-      </div>
-          
-      
-        
+      </div>      
   )
 }
