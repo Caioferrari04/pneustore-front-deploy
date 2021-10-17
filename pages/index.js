7744c566b2edb56e54805078e856ae551028d42d
MarkoPedriali <marko.pedriali@hotmail.com>
2021-10-17 10:44:29 -0300

    estilização da aba montagem movel

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Api } from '../src/Api';
 import Header from '../Components/Header/Header';
 import styles from '../styles/Home.module.css';
+import estilos from '../Components/Tabs/Tabs.module.scss';
 import Card from '../Components/Card/Card';
 import Tabs from '../Components/Tabs/Tabs';
 import ItemCart from '../Components/ItemCart/ItemCart';
@@ -47,9 +48,23 @@ export default function Home() {
       <Frete/>
       <Tabs>
       <div label="Montagem Móvel">
+        <div className="">
           <h2 className={styles.title}>Agende sua entrega com a PneuStore Móvel</h2>
-          <p className={styles.text}>É fácil: você escolhe o pneu, verifica a disponibilidade do
-          serviço, agenda e pronto! Nós entregamos e montamos o pneu sem vocẽ sair de casa </p>
+        </div>
+        <div className={estilos.infoEntrega}>
+          <div className={estilos.info}>
+            <img src="tires.png" alt="Ícone pneu"/>
+            <p className={styles.text}>1. Você escolhe o pneu</p>
+          </div>
+          <div className={estilos.info}>
+            <img src="location2.png" alt="Ícone disponibilidade do serviço"/>
+            <p className={styles.text}>2. Verifica a disponibilidade do serviço</p>
+          </div>
+          <div className={estilos.info}>
+            <img src="van.png" alt="Ícone van de entrega"/>
+            <p className={styles.text}>3. Pronto! Nós entregamos e montamos o pneu sem vocẽ sair de casa </p>
+          </div>
+        </div>
           <Button className={styles.botaoAgendar}variant="outlined">AGENDAR</Button>
       </div>
 
